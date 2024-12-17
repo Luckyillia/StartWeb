3767df95bd67ea520bd04311485138e61edc3e9b
Illia Zaichenko <10376901u@qed.local>
2024-12-17 13:31:04 +0100

    Add new structur to menu bar

--- menu.php
+++ menu.php
@@ -1,5 +1,10 @@
 <?php
 $menu = array(
     "Dashboard" => array ("page" => "index", "name" => "Pulpit"),
-    "Users" => array ("page" => "user", "name" => "Użytkownicy")
+    "Users" => array ("page" => "user", "name" => "Użytkownicy", 
+    'action'=> array(
+        0 => array('action' => 'add', 'title' =>'Dodaj Użytkownika'),
+        1 => array('action' => 'users', 'title' =>'Lista Użytkownika')
+        )
+    )
 );
--- templates/menu.html.php
+++ templates/menu.html.php
@@ -2,6 +2,21 @@
 include('./menu.php');
 echo "<ul class='navbar-nav me-auto mb-2 mb-lg-0'>";
 foreach($menu as $el){
-    echo "<li class='nav-item'><a class='nav-link' href='index.php?page=".$el['page']."'>".$el['name']."</a></li>";
+    if(isset($el['action']) && is_array($el['action'])){
+        foreach($el['action'] as $action){?>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="?page=<?php echo $element['page']; ?>" role="button" data-bs-toggle="dropdown" aria-expanded="false"> <?php echo $element['name']; ?> </a>
+                <ul class="dropdown-menu">
+                    <li>
+                        <a class="nav-link" href="index.php?page=<?php echo $el['pa']
+                    </li>
+                </ul>
+        <?php
+        }
+    }else{
+        echo "<li class='nav-item'>
+                <a class='nav-link' href='index.php?page=".$el['page']."'>".$el['name']."</a>
+            </li>";
+    }
 }
 echo "</ul>";
