c1565acc427ebc3505ca0d0d5edf02c04b30e375
Illia Zaichenko <10376901u@qed.local>
2025-01-28 13:33:20 +0100

    Modify style on news

--- templates/views/news/list.php
+++ templates/views/news/list.php
@@ -1,4 +1,4 @@
-<div class="container border border-primary bg-primary bg-opacity-10 border-3 rounded mx-auto p-2">
+<div class="container">
 	<?php
         $sql = "SELECT * FROM news ORDER BY id ASC";
         $result = $db->query($sql);
@@ -7,9 +7,9 @@
                 $filePath = UPLOAD_PATH . DIRECTORY_SEPARATOR . $row['news_filename'];
                 $imagePath = file_exists($filePath) ? $row['news_filename'] : 'default_img.png';
                 ?>
-                <div>
+                <div class="border border-primary bg-primary bg-opacity-10 border-3 rounded mx-auto p-2 m-3">
                     <div class="text-center">
-                        <img src="<?php echo BASE_URL . 'upload/' . $imagePath; ?>" alt="News Image" class="rounded">
+                        <img src="<?php echo BASE_URL . 'upload/' . $imagePath; ?>" alt="News Image" class="d-block mx-lg-auto img-fluid rounded" width="700" height="500">
                     </div>
                     <div>
                         <h1><?php echo $row['news_title'] ?></h1>
@@ -19,6 +19,10 @@
                     </div>
                     <div>
                         <p><?php echo $row['news_author'] ?></p>
+                    </div> 
+                    <div class="">
+                        <a class='btn btn-primary' href='<?php echo BASE_URL; ?>?page=news&action=edit&id=<?php echo $row['id']; ?>'>Edytuj</a>
+                        <a class='btn btn-danger' onclick='return confirm("Jestes pewny")' href='<?php echo BASE_URL; ?>?page=news&action=drop&id=<?php echo $row['id']; ?>'>Usun</a>
                     </div> 
                 </div>
                 <?php
